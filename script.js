3b10e4eccd962a38818accd6e18f82153ff850c2
Harrison Metz <harrisonmetz17@gmail.com>
2025-11-13 20:14:49 -0900

    added more functions to create player and place marks

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function gameboard() {
+function createGameboard() {
     const row = 3;
     const column = 3;
     let board = [];
@@ -6,7 +6,7 @@ function gameboard() {
     for (let i = 0; i < row; i++) {
         board[i] = [];
         for (let j = 0; j < column; j++){
-            board[i].push('cell');
+            board[i].push(cell());
         }
     }
     
@@ -14,13 +14,61 @@ function gameboard() {
         console.log(board);
     }
 
-    return {logBoard, }
+    const placeMark = function(player, row, column){
+        if (board[row][column] === 0){
+            board[row][column].addMark(player);
+        } else {
+            console.log('That space is already taken...');
+            return;
+        }
+    }
+
+    return {logBoard, placeMark,}
 }
 
-function gameController() {
-    const gameBoard = gameboard();
+function cell() {
+    let value = 0;
 
-    gameBoard.logBoard();
+    const addMark = function(player){
+        return value = player;
+    }
+
+    const getCellValue = function(){
+        return value;
+    }
+}
+
+function gameController(playerOneName, playerOneMark, playerTwoName, playerTwoMark) {
+    const gameboard = createGameboard();
+    
+    const players = [
+        {
+            name : playerOneName,
+            mark : playerOneMark
+        },
+        {
+            name : playerTwoName, 
+            mark : playerTwoMark
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayer = function(){
+        if (activePlayer === players[0]){
+            activePlayer = players[1];
+        } else {
+            activePlayer = players[0];
+        }
+    }
+
+    const getActivePlayer = function(){
+        return activePlayer;
+    }
+
+    gameboard.logBoard();
+
+    
 }
 
 const controller = gameController();
